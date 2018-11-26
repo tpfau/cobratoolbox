525bd89f74c64d310ecbd1710cb5f3de3f7d3b7b
Thomas Pfau <thomas.pfau@uni.lu>
2018-11-26 09:08:16 +0100

    Add docu

--- src/base/utilities/load.m
+++ src/base/utilities/load.m
@@ -1,10 +1,28 @@
 function varargout = load(varargin)
+% This function overrides the matlab builtin load function to automatically
+% convert potential COBRA Models into a valid model struct (if thats even
+% possible).
+% USAGE:
+%    varargout = load(varagin)
+%
+% INPUTS:
+%    varargin:      Normal options for the MATLAB load command (in
+%                   addition, we allow a keyword 'UseOriginalLoad', to
+%                   bypass this functionality.
+%
+% OUTPUTS:
+%    nargout:       The default matlab outputs of load. If no outputs are
+%                   given, than the variables are created in the calling
+%                   workspace.
+%
+% ..Author:         Thomas Pfau 2018
+
 warnstat = warning();
 warning off
 path = fileparts(which(mfilename));
 cleanUp = onCleanup(@() restore(path,warnstat)); 
 rmpath(path);
-if strcmp(varargin{1},'UseOriginalLoad')
+if numel(varargin) > 0 && ischar(varargin{1}) && strcmp(varargin{1},'UseOriginalLoad')
     varargin(1) = [];
     res = load(varargin{:});
     addpath(path); %Make sure that after the load call we are again working with the correct path.
@@ -29,7 +47,11 @@ else
         celem = res.(elements{i});
         if isstruct(celem)
             if all(ismember({'S','rxns','mets'},fieldnames(celem)))
-                celem = convertOldStyleModel(celem);
+                try
+                    celem = convertOldStyleModel(celem);
+                catch
+                    % don't do anything if it doesn't work... 
+                end
                 res.(elements{i}) = celem;
             end
         end
